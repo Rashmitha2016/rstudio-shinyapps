c9a04d7795edad700805dbbdbcfe01c4b79c4dc5
Rashmi <rashmitha1025@gmail.com>
2017-02-25 13:06:15 -0800

    Fixed spacing

--- app2/ui.R
+++ app2/ui.R
@@ -2,7 +2,7 @@ shinyUI(
   pageWithSidebar(
     headerPanel("Body mass index (BMI) calculator"),
     sidebarPanel(
-      numericInput('weight', 'YourWeight (pounds)', 150, min = 0, max = 1000, step = 1),
+      numericInput('weight', 'Your Weight (pounds)', 150, min = 0, max = 1000, step = 1),
       numericInput('height', 'Your Height (inches)', 66, min = 12, max = 108, step = 1),
       submitButton('Calculate')
     ),
